1183e11b5a3033b9b416461f1bb1822a695980db
Chris Gkoumas <cgkoum01@ucy.ac.cy>
2022-08-14 22:33:13 +0300

    Updated Preprocessing_script.py
    added to do for the calculation of the alerting network score.

--- Child ANT/Analysis/Preprocessing_script.py
+++ Child ANT/Analysis/Preprocessing_script.py
@@ -21,11 +21,11 @@ columns_to_keep = ["Cue",
                    "response.corr",
                    "response.rt",
                    "participant"]
-                   
+
 datafile = datafile[datafile.columns[datafile.columns.isin(columns_to_keep)]]
 
 
-# Remove rows where Accuracy = 0, as they are not included in the calculation 
+# Remove rows where Accuracy = 0, as they are not included in the calculation
 # of reaction times (RT)
 RT_df = datafile[datafile["response.corr"] == 1]
 descriptives = ['min', 'max', 'median', 'std', 'sem']
@@ -40,11 +40,11 @@ alerting_RT_median = no_cue_RT - double_cue_RT
 fig = plt.figure(figsize = (11, 6))
 plt.bar(scores.index ,
         scores.iloc[:,2],
-        color ='maroon', 
+        color ='maroon',
         yerr = scores.iloc[:,4],
         width = 0.4,
         capsize = 10)
- 
+
 plt.xlabel("Condition")
 plt.ylabel("Reaction Time (in ms)")
 plt.title("Alerting network score")
@@ -61,3 +61,5 @@ executive =  RT_df.groupby("Target_side")["response.rt"].agg(descriptives)
 congruent_RT = executive.iloc[0,2]
 incogruent_RT = executive.iloc[1,2]
 executive_RT_median = incogruent_RT - congruent_RT
+
+#TO DO: Add calculation for alerting network.
